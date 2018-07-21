b7c989eef5002f42fb6e00f1b3cb88c7de7309c7
Koce <koce.zhao@gmail.com>
2018-07-21 11:55:37 +0800

    Initial Commit

---
+++ KCRouter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KCRouter
+//
+//  Created by Koce on 2018/7/21.
+//  Copyright © 2018 Koce. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
